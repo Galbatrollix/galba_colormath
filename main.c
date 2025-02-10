90f83688690504f3822bcbb4d733763b744d4d9d
Galbatrollix <andzielinski@gazeta.pl>
2025-02-10 12:54:54 +0100

    cleaned up makefile

--- main.c
+++ main.c
@@ -36,22 +36,29 @@ bool (*test_functions[TEST_COUNT])(void) = {
     test_premade_XYZ_RGB
 };
 
+static void print_xyz(xyz_t p){
+    printf("xyz(%lf %lf %lf)", p.x, p.y, p.z);
+}
 static void print_rgb(rgb_t p){
     printf("rgb(%u %u %u)", p.r, p.g, p.b);
 }
+static void print_lab(lab_t p){
+    printf("lab(%lf %lf %lf)", p.l, p.a, p.b);
+}
+
 int main(void){
 
     int tests_succeeded = 0;
 
-    for(int i=0; i<TEST_COUNT; i++){
-        tests_succeeded += test_functions[i]();
+    // for(int i=0; i<TEST_COUNT; i++){
+    //     tests_succeeded += test_functions[i]();
 
-    }
+    // }
 
     printf("All tests completed.\nSuccessful tests: %d/%d\n", tests_succeeded, TEST_COUNT);
 
-
-
+    print_xyz(XYZ_from_RGB((rgb_t){255,255,255}));
+    print_rgb(RGB_from_XYZ((xyz_t){95, 123, 109}));
 
 }
 
--- makefile
+++ makefile
@@ -1,4 +1,5 @@
-SHELL = cmd
+#uncomment the below if using mingw
+#SHELL = cmd
 
 APP_NAME = program
 
@@ -31,19 +32,5 @@ $(APP_NAME).exe: $(C_OBJ_FILES) $(C_HEADERS) Makefile
 	$(C_COMPILER) $(C_FLAGS) $(C_OBJ_FILES) -o $(APP_NAME).exe
 
 
-clean:
-	del /Q /F $(APP_NAME).exe
-	del /Q /F $(C_SOURCE_FOLDER)\\*.o
-	del /Q /F $(TEST_FOLDER)\\*.o
-	del /Q /F main.o
-
-run: $(APP_NAME).exe
-	$(APP_NAME).exe
-
-# target is any .o file and its prerequisite is a .c file
-# -c means that gcc should compile but NOT link the files
-# https://stackoverflow.com/questions/3220277/what-do-the-makefile-symbols-and-mean
-# $< means <name of fist prerequisite> here %.c
-# $@ means <name of target> here %.o
 %.o: %.c Makefile
 	$(C_COMPILER) $(C_FLAGS) -c $< -o $@
